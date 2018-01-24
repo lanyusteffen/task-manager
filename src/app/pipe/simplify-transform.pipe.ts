5631cde38fe4ec0638cd3a0eec0671af04427f2e
lanyusteffen <lanyusteffen@gmail.com>
2018-01-24 18:03:02 +0800

    修复精灵程序更新后

--- src/app/pipe/simplify-transform.pipe.ts
+++ src/app/pipe/simplify-transform.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'simplifyTransform'})
 export class SimplifyTransformPipe implements PipeTransform {
-    transform(content: string, maxLength: number, addEllipsis: boolean): string {
+    transform(content: string, maxLength?: number, addEllipsis?: boolean, ellipsisAdditional: string = '...'): string {
         if (content.length > maxLength) {
             var transformContent = content.substring(0, maxLength);
             if (addEllipsis) {
-                transformContent = transformContent + '......';
+                transformContent = transformContent + ellipsisAdditional;
             }
             return transformContent;
         }
--- src/app/warning/warning.module.ts
+++ src/app/warning/warning.module.ts
@@ -5,12 +5,14 @@ import { WarningDetailComponent } from './warning-detail/warning-detail.componen
 import { WarningRoutingModule } from './warning-routing.module';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { PipeModule } from '../pipe/pipe.module';
 
 @NgModule({
   imports: [
     CommonModule,
     WarningRoutingModule,
     NgxDatatableModule,
+    PipeModule,
     ModalModule.forRoot()
   ],
   declarations: [
